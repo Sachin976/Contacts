461caf91d7e78b4113cee7fe8ef5bfe30af36150
Sachin976 <83978822+Sachin976@users.noreply.github.com>
2021-06-21 22:57:02 +0530

    update

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose')
-const connectionURL = "mongodb+srv://Sachin97659:Sachin@27_99@cluster0.edxeh.mongodb.net/Contacts?retryWrites=true&w=majority"
+const connectionURL = "mongodb+srv://Sachin97659:<password>@cluster0.edxeh.mongodb.net/Contacts?retryWrites=true&w=majority"
 mongoose.connect(connectionURL,{
     useNewUrlParser:true,
     useUnifiedTopology:true,
